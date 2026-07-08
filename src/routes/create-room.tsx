e4c44d408cd575571e056ee7292451f16fea3dc1
pradeepintuitive-source <pradeepintuitiveapps@gmail.com>
2026-07-08 14:00:09 +0000

    create-room

--- src/routes/create-room.tsx
+++ src/routes/create-room.tsx
@@ -37,7 +37,7 @@ function CreateRoomPage() {
 
   const submit = async (e: React.FormEvent) => {
     e.preventDefault();
-    console.log("Submit clicked and checked");
+    console.log("Submit clicked");
     setLoading(true);
     try {
       if (!auth.user) {
